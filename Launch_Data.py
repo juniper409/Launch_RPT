e5234ec0099e3fa2478e9e100e47e0707eb46508
“MrJuniper409” <mrjuniper409@gmail.com>
2023-02-12 13:09:24 -0500

    Added code to write report to file

--- Launch_Data.py
+++ Launch_Data.py
@@ -1,31 +1,33 @@
+import datetime
 import requests
 import json
 
 response = requests.get('https://lldev.thespacedevs.com/2.2.0/launch/upcoming/')
-
 status = response.status_code
-x = 0
+f = open("list.txt","a")
 
 #Gives error code if the connection fails.
 if status != 200:
     print("Error retrieving data -", status)
+    f.write("Error retrieving data -", status)
 else:
     pass
-
+    x=0
 while x <= 10:
     #Retrieves the needed info from the API and assigns variable.
     name = response.json()['results'][x]['name']
     date = response.json()['results'][x]['net']
     location = response.json()['results'][x]['pad']['name']
     launch_description = response.json()['results'][x]['mission']['description']
-    
+        
     #Print Data
-    print(f'----- {name} -----')
-    print(f'Launch D&T: {date}')
-    print(f'From: {location}')
-    print()
+    f.writelines(f'----- {name} -----\n')
+    f.writelines(f'Launch D&T: {date}\n')
+    f.writelines(f'From: {location}\n')
+    f.write("\n")
     x += 1
     if x == 10:
+        f.close()
         break
-    
-        
+            
+print("REPORT CREATION COMPLETE")
